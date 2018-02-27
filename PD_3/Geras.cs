67d314f7ccaa4c7b911f91ccc94b265e43f74cbd
Mykolas Jogurtas <miik4s@outlook.com>
2018-02-27 10:07:46 +0200

    pridejau metoda

--- PD_3/Geras.cs
+++ PD_3/Geras.cs
@@ -10,8 +10,10 @@ namespace PD_3
         private int x = 9;
 
         private int valio = 6;
-        //ttttt
 
-        //labas
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
     }
 }
